e865109e74f24fdf2a443bafedb3657595328058
herokuuser11 <herokuuser11@>
2018-02-12 19:15:10 +0300

    settings 2 cfg

--- examples/hello-world.js
+++ examples/hello-world.js
@@ -1,5 +1,16 @@
+
 const TeleBot = require('../');
-const bot = new TeleBot('TELEGRAM_BOT_TOKEN');
+
+const token = require('../cfg/set.json').token
+
+const bot = new TeleBot({
+    token: token, // Required. Telegram Bot API token.
+    polling: { // Optional. Use polling.
+        interval: 2000, // Optional. How often check updates (in ms).
+        timeout: 0, // Optional. Update polling timeout (0 - short polling).
+        limit: 100, // Optional. Limits the number of updates to be retrieved.
+        retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
+    }});
 
 // On every text message
 bot.on('text', msg => {
